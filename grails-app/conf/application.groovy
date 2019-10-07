5e7420428be9a74ec68ed1f73fb781d344aa29a5
tachi123 <tachi123@msn.com>
2019-10-07 17:26:05 -0300

    se arregla explora tu area. incompatibilidad con jquery-migrate

--- grails-app/conf/application.groovy
+++ grails-app/conf/application.groovy
@@ -13,7 +13,8 @@
 
 grails.project.groupId = "au.org.ala" // change this to alter the default package name and Maven publishing destination
 
-default_config = "/data/${appName}/config/${appName}-config.properties"
+default_config= "C:\\data\\${appName}\\config\\${appName}-config.properties"
+//default_config = "/data/${appName}/config/${appName}-config.properties"
 if(!grails.config.locations || !(grails.config.locations instanceof List)) {
     grails.config.locations = []
 }
